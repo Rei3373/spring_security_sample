f70a1f538b9d87c0f199c8a4f291987eb5419444
Rei3373 <152368897+Rei3373@users.noreply.github.com>
2023-12-22 12:39:50 +0900

    LESSON05

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -16,9 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 認証・認可に関する設定を追加します
     	// 認可の設定
-        http.authorizeRequests()
-            .antMatchers("/loginForm").permitAll() // /loginFormは、全ユーザからのアクセスを許可
-            .anyRequest().authenticated(); // 許可した項目以外は、認証を求める
+    	http.authorizeRequests()
+        .antMatchers("/loginForm").permitAll()
+        .antMatchers("/admin").hasAuthority("ADMIN") // 管理者のみ/adminにアクセスできる
+        .anyRequest().authenticated();
         
       //ログイン処理
         http.formLogin()
